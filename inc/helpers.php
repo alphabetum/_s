757d98671b26809de480977c70584d11e0e852d1
William Melody <hi@williammelody.com>
2017-04-02 16:09:07 -0700

    Add spacing around PHP function comments.
    Better adhere to WordPress PHP coding standards.

--- inc/helpers.php
+++ inc/helpers.php
@@ -9,8 +9,8 @@
  * Print the path of a file in the 'images' directory with the theme version
  * appended at the end.
  */
-function get_theme_image( $theme_image) {
-	$image_uri = get_theme_file_uri('assets/images/' . $theme_image);
+function get_theme_image( $theme_image ) {
+	$image_uri = get_theme_file_uri( 'assets/images/' . $theme_image );
 	echo $image_uri . '?v=' . wp_get_theme()->get( 'Version' );
 }
 
