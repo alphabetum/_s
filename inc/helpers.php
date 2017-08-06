c69428621cbaebf590f48a165c610886fcbd228f
William Melody <hi@williammelody.com>
2017-08-05 22:28:18 -0700

    Use `return` rather than `echo` in helpers.php.

--- inc/helpers.php
+++ inc/helpers.php
@@ -6,11 +6,11 @@
  */
 
 /**
- * Print the path of a file in the 'images' directory with the theme version
+ * Return the path of a file in the 'images' directory with the theme version
  * appended at the end.
  */
-function get_theme_image( $theme_image ) {
+function get_theme_image_uri( $theme_image ) {
 	$image_uri = get_theme_file_uri( 'assets/images/' . $theme_image );
-	echo $image_uri . '?v=' . wp_get_theme()->get( 'Version' );
+	return $image_uri . '?v=' . wp_get_theme()->get( 'Version' );
 }
 
